72534e56f6759ec82d6bffdc17e28ec89f252118
Fabulash7 <wizzlin1993@gmail.com>
2022-12-09 00:53:57 -0800

    Task 7 Tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char r;
+
+	for (r = 'z'; r >= 'a'; r--)
+		putchar(r);
+
+	putchar('\n');
+
+	return (0);
+
+}
