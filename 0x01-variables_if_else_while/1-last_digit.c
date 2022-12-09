be1a8618a8825f547926414be415901af7c62035
Fabulash7 <wizzlin1993@gmail.com>
2022-12-09 15:14:48 -0800

    Task 9 comb

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - main block
+ * Description: get a random number and check its last digit with 5 
  * Return: Always 0
  */
 int main(void)
@@ -19,7 +20,7 @@ int main(void)
 		printf("Last digit of %d is %d and is greater than 5\n", n, a);
 	else if (a == 0)
 		printf("Last digit of %d is %d and is 0\n", n, a);
-	else
+	else if (a < 6)
 		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, a);
 	return (0);
 
