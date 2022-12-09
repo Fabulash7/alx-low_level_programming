3b9078c0c2aac8e553ec812cead0ef2ec39324d9
Fabulash7 <wizzlin1993@gmail.com>
2022-12-09 01:07:03 -0800

    Task 9 Comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int m;
+
+	for (m = 48; m < 58; m++)
+		putchar(m);
+		if (m != 57)
+			putchar(',');
+			putchar(' ');
+
+	putchar('\n');
+
+	return (0);
+
+}
