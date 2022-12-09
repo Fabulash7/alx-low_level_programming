3385273bb899012d99fba2dbc535ec95314853ba
Fabulash7 <wizzlin1993@gmail.com>
2022-12-09 00:19:11 -0800

    Task 2 Alphabets

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char pc;
+
+	for (pc = 'a'; pc <= 'z'; pc++)
+	{
+		putchar(pc);
+	}
+	putchar('\n');
+	return (0);
+
+}
