1e7881bc8ffd472446ec577101e2e786edb23148
Fabulash7 <wizzlin1993@gmail.com>
2022-12-09 00:46:26 -0800

    Task 6 Numbers

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int numb;
+
+	for (numb = 0; numb < 10; numb++)
+		putchar((numb % 10) + '0');
+
+	putchar('\n');
+
+	return (0);
+
+}
