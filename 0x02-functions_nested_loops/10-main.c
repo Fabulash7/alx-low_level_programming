d4f8bc4139dba6fad2bb90c516f10b1fafc1ffdc
Fabulash7 <wizzlin1993@gmail.com>
2022-12-13 18:18:38 -0800

    Task 10 add

---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	int b;
+
+	b = add(89, 9);
+	printf("%d\n", b);
+	return (0);
+}
